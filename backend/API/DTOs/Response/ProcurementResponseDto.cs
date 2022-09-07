5ae9d99f627105e3bb72dda7573a68720b9d7b47
Tausif Ahmed <tausifahmed0235@gmail.com>
2022-09-07 22:20:50 +0600

    procurement database updated

--- backend/API/DTOs/Response/ProcurementResponseDto.cs
+++ backend/API/DTOs/Response/ProcurementResponseDto.cs
@@ -1,26 +1,20 @@
 ﻿using API.Entities;
 using System.ComponentModel.DataAnnotations;
 
-namespace API.DTOs.Request
+namespace API.DTOs.Response
 {
-    public class ProcuremnetDto
+    public class ProcurementResponseDto
     {
-        [Required]
         public string Name { get; set; }
 
-        [Required]
         public string Category { get; set; }
 
-        [Required]
         public float EstimatedTotalPrice { get; set; }
 
-        [Required]
         public DateTime IssuingDate { get; set; }
 
-        [Required]
         public DateTime TenderDeadline { get; set; }
 
-        [Required]
         public List<ProcurementItem> Items { get; set; }
     }
 }
--- backend/API/Entities/ProcurementItem.cs
+++ backend/API/Entities/ProcurementItem.cs
@@ -3,7 +3,19 @@
     public class ProcurementItem
     {
         public int Id { get; set; }
-        
-        
+
+        public string Name { get; set; }
+
+        public ProductCategory Category { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Details { get; set; }
+
+        public float EstimatedPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public float EstimatedTotalPrice { get; set; }
     }
 }
